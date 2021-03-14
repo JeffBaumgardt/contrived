4677c986873b8d5a9b708b7bb2a63fed7851bd0a
Jeff Baumgardt <medros1455@gmail.com>
2021-03-13 20:36:20 -0700

    Prepare for talk

--- src/__tests__/Main.test.tsx
+++ src/__tests__/Main.test.tsx
@@ -1,20 +1,10 @@
 import * as React from 'react'
 import {Routes, Route, MemoryRouter as Router} from 'react-router-dom'
-import {render, screen, waitFor, waitForElementToBeRemoved, within} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {setupServer} from 'msw/node'
-import {graphql} from 'msw'
+import {render, screen} from '@testing-library/react'
 
 import GraphqlContext from './helpers/GraphqlContext'
-import * as queryData from './helpers/rocketData.json'
 
 import Main from '../Main'
-
-const server = setupServer()
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
 
 const App = ({children}: {children: React.ReactNode}) => {
     return (
@@ -28,7 +18,9 @@ describe('Main', () => {
     test('Loads with initial table', () => {
         render(
             <App>
-                <Main />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                </Routes>
             </App>,
         )
 
@@ -38,58 +30,16 @@ describe('Main', () => {
     test('Should be initially loading', () => {
         render(
             <App>
-                <Main />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                </Routes>
             </App>,
         )
 
         expect(screen.getByText('Loading...')).toBeInTheDocument()
     })
 
-    test('Should load up with some rockets', async () => {
-        server.use(
-            graphql.query('GetRockets', (req, res, ctx) => {
-                return res(ctx.data(queryData.rockets))
-            }),
-        )
+    test.skip('Should load up with some rockets', () => {})
 
-        render(
-            <App>
-                <Main />
-            </App>,
-        )
-
-        await waitFor(() => expect(screen.getByText('Loading...')).toBeInTheDocument())
-        await waitForElementToBeRemoved(screen.getByText('Loading...'))
-
-        const rows = queryData.rockets.rockets
-
-        rows.forEach(rocket => {
-            const row = screen.getByText(rocket.name).closest('tr')
-            const utils = within(row as HTMLTableRowElement)
-
-            expect(utils.getByText(rocket.name)).toBeInTheDocument()
-            expect(utils.getByText(rocket.description)).toBeInTheDocument()
-        })
-    })
-
-    test('Should be able to click on a row to navigate', async () => {
-        server.use(
-            graphql.query('GetRockets', (req, res, ctx) => {
-                return res(ctx.data(queryData.rockets))
-            }),
-        )
-
-        render(
-            <App>
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/starship" element={<span>Navigated</span>} />
-                </Routes>
-            </App>,
-        )
-
-        userEvent.click(screen.getByText('Starship'), undefined, {skipHover: true})
-
-        await waitFor(() => expect(screen.getByText('Navigated')).toBeInTheDocument())
-    })
+    test.skip('Should be able to click on a row to navigate', () => {})
 })
--- src/__tests__/Ship.test.tsx
+++ src/__tests__/Ship.test.tsx
@@ -1,19 +1,10 @@
 import * as React from 'react'
 import {Routes, Route, MemoryRouter as Router} from 'react-router-dom'
-import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
-import {setupServer} from 'msw/node'
-import {graphql} from 'msw'
+import {render, screen} from '@testing-library/react'
 
 import GraphqlContext from './helpers/GraphqlContext'
-import * as queryData from './helpers/rocketData.json'
 
 import Ship from '../Ship'
-
-const server = setupServer()
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
 
 const App = ({children}: {children: React.ReactNode}) => {
     return (
@@ -27,30 +18,14 @@ describe('Ship', () => {
     test('Loads with a loading bar', () => {
         render(
             <App>
-                <Ship />
-            </App>,
-        )
-
-        expect(screen.getByTestId('loader')).toBeInTheDocument()
-    })
-
-    test('Should load up with data', async () => {
-        server.use(
-            graphql.query('GetRocket', (req, res, ctx) => {
-                return res(ctx.data(queryData.rocket))
-            }),
-        )
-
-        render(
-            <App>
                 <Routes>
                     <Route path="/:rocketId" element={<Ship />} />
                 </Routes>
             </App>,
         )
 
-        await waitForElementToBeRemoved(screen.getByTestId('loader'))
-
-        expect(screen.getByText('Starship')).toBeInTheDocument()
+        expect(screen.getByTestId('loader')).toBeInTheDocument()
     })
+
+    test.skip('Should load up with data', () => {})
 })
